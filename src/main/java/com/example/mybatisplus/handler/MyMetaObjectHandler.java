3e61cbc27e5ea2ac715432924fc85ec343668502
lqd <lqd@guigu.com>
2021-09-08 13:38:25 +0800

    hot-fix commit

--- src/main/java/com/example/mybatisplus/handler/MyMetaObjectHandler.java
+++ src/main/java/com/example/mybatisplus/handler/MyMetaObjectHandler.java
@@ -16,6 +16,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         this.setFieldValByName("createTime", new Date(), metaObject);
         this.setFieldValByName("updateTime", new Date(), metaObject);
         System.out.println("111");
+        System.out.println("222");
     }
 
     @Override
